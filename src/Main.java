dc1ba489e5439c6634111e1ca9d315499803df1a
Ksenia Bespalova <bespalovaksenya9@gmail.com>
2026-03-16 14:27:31 +0300

    Primitives

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-
-        int amount = 14_588; // стоимость билета
-        int bonusUnit = 20; // количество рублей для одной бонусной мили
-
-        System.out.println("Количество начисленных миль за купленный билет:");
-        System.out.println((amount / bonusUnit) + " бонусов");
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
 }
